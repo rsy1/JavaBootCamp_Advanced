dddb73f3dc247a0cf0098a6700ac0191e79939e0
rsy1 <royal.siyanata@gmail.com>
2022-09-01 13:45:23 +0200

    daily_commit

---
+++ src/bk2_07Methods/HelloWorldMethod.java
@@ -0,0 +1,12 @@
+package bk2_07Methods;
+//Illustrations: method declaration
+
+public class HelloWorldMethod {
+	public static void main(String[] args) {
+		sayHello();
+	}
+
+	public static void sayHello() {
+		System.out.println("Hello, World!");
+	}
+}
---
+++ src/bk2_07Methods/ParameterScope.java
@@ -0,0 +1,17 @@
+package bk2_07Methods;
+
+//Scoping out parameters
+
+public class ParameterScope {
+	public static void main(String[] args) {
+		int min = 1;
+		int max = 10;
+		int number = getRandomNumber(min, max);
+		System.out.println(number);
+	}
+
+	public static int getRandomNumber(int min, int max) {
+		return (int) (Math.random() * (max - min + 1)) + min;
+	}
+
+}
