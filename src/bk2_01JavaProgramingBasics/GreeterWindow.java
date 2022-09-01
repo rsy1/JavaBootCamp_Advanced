66a69584257230db56285a4ca5510a4f59e18a3c
rsy1 <royal.siyanata@gmail.com>
2022-09-01 11:50:22 +0200

    geeterWindow

---
+++ src/bk2_01JavaProgramingBasics/GreeterWindow.java
@@ -0,0 +1,11 @@
+package bk2_01JavaProgramingBasics;
+
+import javax.swing.JOptionPane;
+
+public class GreeterWindow {
+	
+	public void sayHello() {
+		JOptionPane.showMessageDialog(null,"Hello, World!", "Greeter", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
