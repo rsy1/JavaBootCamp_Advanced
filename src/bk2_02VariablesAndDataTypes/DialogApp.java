7a8888669152d7dc8e2cc5ff297b2475857ec0be
rsy1 <royal.siyanata@gmail.com>
2022-09-01 12:15:45 +0200

    daily_commit

---
+++ src/bk2_02VariablesAndDataTypes/DialogApp.java
@@ -0,0 +1,12 @@
+package bk2_02VariablesAndDataTypes;
+
+import javax.swing.JOptionPane;
+
+public class DialogApp {
+	public static void main(String[] args) {
+		String s;
+		s = JOptionPane.showInputDialog("Enter an integer:");
+		int x = Integer.parseInt(s);
+		System.out.println("You entered " + x + ".");
+	}
+}
