7038101758b9a8246a4af8e22cdb865c2fff79da
rsy1 <royal.siyanata@gmail.com>
2022-09-01 11:51:57 +0200

    HelloApp2

---
+++ src/bk2_01JavaProgramingBasics/HelloApp2.java
@@ -0,0 +1,10 @@
+package bk2_01JavaProgramingBasics;
+
+public class HelloApp2 {
+	public static void main(String[] args) {
+		//Greeter myGreeterObject = new Greeter();
+		GreeterWindow myGreeterWindowObject = new GreeterWindow();
+		myGreeterWindowObject.sayHello();
+		//System.out.println(myGreeterObject.sayHello());
+	}
+}
