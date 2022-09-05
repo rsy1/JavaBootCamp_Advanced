d73567f8b4908421a2ba607e2f816266fd506d58
rsy1 <royal.siyanata@gmail.com>
2022-09-05 10:18:15 +0200

    daily commit

---
+++ src/bk3_04UsingSubClassesAndInterfaces/BouncingBall.java
@@ -0,0 +1,11 @@
+package bk3_04UsingSubClassesAndInterfaces;
+
+public class BouncingBall extends Ball{
+
+	// methods and fields that add the ability to bounce
+	// to a basic Ball object:
+	public void bounce() {
+		// the bounce method
+	}
+
+}
---
+++ src/bk3_04UsingSubClassesAndInterfaces/Chess.java
@@ -0,0 +1,8 @@
+package bk3_04UsingSubClassesAndInterfaces;
+
+public class Chess extends Game{
+	public void play() {
+		System.out.println("I give up. You win.");
+	}
+
+}
