558e4217871a27a41d515dbb17cc22040a1e20c1
rsy1 <royal.siyanata@gmail.com>
2022-09-05 10:14:13 +0200

    daily commit

---
+++ src/bk3_03WorkingWithStatics/TestClass.java
@@ -0,0 +1,9 @@
+package bk3_03WorkingWithStatics;
+
+public class TestClass {
+	private int x = 5; // an instance field
+
+	public static void main(String[] args) {
+		int y = x; // e
+	}
+}
---
+++ src/bk3_03WorkingWithStatics/Validation.java
@@ -0,0 +1,6 @@
+package bk3_03WorkingWithStatics;
+
+public class Validation {
+	private Validation() {} // prevents instances
+	// static methods and fields go here
+}
