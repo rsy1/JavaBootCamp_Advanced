47675013e627ffe3353b0f68b327236a193bade6
rsy1 <royal.siyanata@gmail.com>
2022-09-07 17:01:33 +0200

    daily commit

---
+++ src/bk4_01WorkingWithString/ListWords.java
@@ -0,0 +1,15 @@
+package bk4_01WorkingWithString;
+
+import java.util.Scanner;
+
+public class ListWords {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.print("Enter a string: ");
+		String s = sc.nextLine();
+		String[] word = s.split("\\s+");
+		for (String w : word)
+			System.out.println(w);
+	}
+}
