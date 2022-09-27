5061bf97f753252f9bb64e830b13fc70df2730f2
rsy1 <royal.siyanata@gmail.com>
2022-09-27 13:44:01 +0200

    daily comit

---
+++ src/bk5_01ProgrammingThreads/DoTwoThings.java
@@ -0,0 +1,18 @@
+package bk5_01ProgrammingThreads;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+public class DoTwoThings {
+	ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(2);
+	CountDownClock clock = new CountDownClock(20);
+
+	public static void main(String[] args) {
+		new DoTwoThings();
+	}
+
+	DoTwoThings() {
+		pool.execute(clock);
+		pool.execute(clock);
+		pool.shutdown();
+	}
+}
