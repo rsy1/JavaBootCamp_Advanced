ab33a700c7fc018829521d2f06d86506cd218cea
rsy1 <royal.siyanata@gmail.com>
2022-10-25 16:34:14 +0200

    daily commit

---
+++ src/bk8_03The_Database/friends.sql
@@ -0,0 +1,16 @@
+create table friend (
+lastname varchar(50),
+firstname varchar(50),
+movieid int
+);
+
+insert into friend (lastname, firstname, movieid)
+values ("Haskell", "Eddie", 3);
+insert into friend (lastname, firstname, movieid)
+values ("Haskell", "Eddie", 5);
+insert into friend (lastname, firstname, movieid)
+values ("Cleaver", "Wally", 9);
+insert into friend (lastname, firstname, movieid)
+values ("Mondello", "Lumpy", 2);
+insert into friend (lastname, firstname, movieid)
+values ("Cleaver", "Wally", 3);
