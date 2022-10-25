c1b0aae4e3ef232a816af22fb57acd798ff5173e
rsy1 <royal.siyanata@gmail.com>
2022-10-25 16:09:14 +0200

    daily commit

---
+++ src/bk6_01JavaFX_Introduction/HelloController.java
@@ -0,0 +1,14 @@
+package bk6_01JavaFX_Introduction;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
